f2fd570965b55c0274283483ad403b89a6bb9789
David Powell <30627991+Zinoxate@users.noreply.github.com>
2017-11-21 09:56:48 -0500

    Add files via upload

---
+++ tribute/Tribute.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $(".show").click(function(){
+        $(".Picture").toggle(2500);
+    })
+});
+
+$(document).ready(function(){
+    $(".table").click(function(){
+        $(".Timeline").toggle(2500);
+    })
+});
+
+$(document).ready(function(){
+    $(".info").click(function(){
+        $(".Information").toggle(2500);
+    })
+});
+
+$(document).ready(function(){
+    $(".show").click(function(){
+        $(".placeholder").toggle(2500);
+    })
+});
+
+$(document).ready(function(){
+    $(".info").click(function(){
+        $(".Timeline").hide(2500);
+    })
+});
+
+
